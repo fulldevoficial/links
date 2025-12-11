74ce84f590e556878ddacf61e06bf14a115f1657
Lucas Pedro <lucasdearaujopedrolap@gmail.com>
2025-12-11 09:48:12 -0300

    .

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 import { environment } from '../../../environments/environment';
